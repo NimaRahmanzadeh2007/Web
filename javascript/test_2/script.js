46dfff11e18bb1ceedd3c909ca973409c017d956
NimaRahmanzadeh2007 <nimarahmanzadeh2007@gmail.com>
2026-07-28 09:42:01 +0330

    creating repository

---
+++ javascript/test_2/script.js
@@ -0,0 +1,20 @@
+const button = document.getElementById("btn");
+
+button.onclick = function () {
+    
+    const colors = [
+        "red",
+        "blue",
+        "green",
+        "purple",
+        "orange",
+        "pink",
+        "black",
+        "gold"
+    ]
+
+    const random = Math.floor(Math.random()*colors.length);
+
+    document.body.style.backgroundColor = colors[random];
+
+}
