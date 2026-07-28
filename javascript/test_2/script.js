6238aac9cee494c2a744f21c8023e0cbd06301b5
NimaRahmanzadeh2007 <nimarahmanzadeh2007@gmail.com>
2026-07-28 13:37:56 +0330

    random color project

--- javascript/test_2/script.js
+++ javascript/test_2/script.js
@@ -1,22 +1,33 @@
 const button = document.getElementById("btn");
 
+const title = document.getElementById("title");
+
+const colors = [
+    "red",
+    "blue",
+    "green",
+    "purple",
+    "orange",
+    "pink",
+    "black",
+    "gold"
+]
+
+
+
 button.onclick = function () {
-    
-    const colors = [
-        "red",
-        "blue",
-        "green",
-        "purple",
-        "orange",
-        "pink",
-        "black",
-        "gold"
-    ]
 
-    const random = Math.floor(Math.random()*colors.length);
+    const random = Math.floor(Math.random() * colors.length);
 
-    document.body.style.backgroundColor = colors[random]
+    document.body.style.backgroundColor = colors[random];
 
-    
+    if (document.body.style.backgroundColor == "black") {
+        title.style.color = "white";
+    }
+    else {
+        title.style.color = "black";
+    }
+
+    button.textContent = colors[random];
 
 }
