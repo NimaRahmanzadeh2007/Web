956b60ebc2d7ef09e24e92b1dcded3ead1d7f0ca
NimaRahmanzadeh2007 <nimarahmanzadeh2007@gmail.com>
2026-07-30 12:34:21 +0330

    Todo List Project

---
+++ javascript/Todo_List/script.js
@@ -0,0 +1,31 @@
+const btn = document.getElementById("btn");
+const input = document.getElementById("input");
+
+btn.addEventListener("click", function () {
+
+    if(input.value.trim() !== "")
+    {
+        const task = document.createElement("div")
+        task.className = "task";
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+
+        task.appendChild(checkbox);
+
+        const text = document.createElement("p");
+        text.className = "checkItemTextStyle"
+
+        text.innerText = input.value.trim();
+
+        task.appendChild(text);
+
+        document.getElementById("list").appendChild(task);
+
+        input.value = "";
+    }
+    else{
+        window.alert("Please type a task first!");
+    }
+
+});
