3f0888a818dbde63d17198a9b37cd8717a742018
NimaRahmanzadeh2007 <nimarahmanzadeh2007@gmail.com>
2026-08-01 08:55:54 +0330

    Digital Clock project

---
+++ javascript/Digital_Clock/script.js
@@ -0,0 +1,24 @@
+const timeText = document.getElementById("timeText");
+
+function updateClock() {
+
+    const now = new Date();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    timeText.textContent = hours + " : " + minutes + " : " + seconds;
+}
+updateClock();
+setInterval(updateClock, 1000);
