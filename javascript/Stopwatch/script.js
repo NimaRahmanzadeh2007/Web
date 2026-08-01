2758d4ce7f10cacab21916547fbb8f68f74eff16
NimaRahmanzadeh2007 <nimarahmanzadeh2007@gmail.com>
2026-08-01 10:18:39 +0330

    start and stop

--- javascript/Stopwatch/script.js
+++ javascript/Stopwatch/script.js
@@ -1,16 +1,44 @@
 const timeText = document.getElementById("time");
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
 
-let milliseconds = 0;
-let  = 0;
+let centiseconds = 0;
+let seconds = 0;
 let minutes = 0;
 
 function stopwatch() {
 
-    seconds++;
+
+    centiseconds++;
+
+    if (centiseconds > 99) {
+        centiseconds = 0;
+        seconds += 1;
+    }
+    if (seconds > 59) {
+        seconds = 0;
+        minutes += 1;
+    }
 
 
-    timeText.textContent = `${minutes}:${seconds}:${milliseconds}`;
+    timeText.textContent = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}:${centiseconds.toString().padStart(2, "0")}`;
 
 }
+
+let timer;
+
+function stopwatchStart() {
+    if (!timer) {
+        timer = setInterval(stopwatch, 10);
+    }
+}
+function stopwatchStop() {
+    clearInterval(timer);
+    timer = null;
+}
+
+startBtn.onclick = stopwatchStart;
+stopBtn.onclick = stopwatchStop;
 
 
