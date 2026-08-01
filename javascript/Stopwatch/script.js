de626a9b432a07d4acdc17ca8d0b8656334bb9b6
NimaRahmanzadeh2007 <nimarahmanzadeh2007@gmail.com>
2026-08-01 09:52:33 +0330

    creating Stopwatch

---
+++ javascript/Stopwatch/script.js
@@ -0,0 +1,16 @@
+const timeText = document.getElementById("time");
+
+let milliseconds = 0;
+let  = 0;
+let minutes = 0;
+
+function stopwatch() {
+
+    seconds++;
+
+
+    timeText.textContent = `${minutes}:${seconds}:${milliseconds}`;
+
+}
+
+
