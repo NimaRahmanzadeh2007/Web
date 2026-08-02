44dbda4e4fc72e93efe6b5f4b6df56a30ccc49f5
NimaRahmanzadeh2007 <nimarahmanzadeh2007@gmail.com>
2026-08-02 11:41:26 +0330

    caculating the sum and displaying on the screen

--- javascript/Caculator/script.js
+++ javascript/Caculator/script.js
@@ -1,1 +1,24 @@
-const clearBtn = document.getElementById("clearBtn");
+const buttons = document.querySelectorAll("button");
+const display = document.querySelector(".display");
+
+let currentInput = "";
+
+buttons.forEach(function (button) {
+
+    button.addEventListener("click", function (event) {
+
+        const value = event.target.innerText;
+
+        if (value == "=") {
+            const result = eval(currentInput);
+            display.innerText = result;
+            currentInput = result;
+            return
+        }
+
+        currentInput += value;
+        display.innerText = currentInput;
+
+    });
+
+});
