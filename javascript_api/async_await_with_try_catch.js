db98cd162ab9826cafc50278af3d41a91a37286b
NimaRahmanzadeh2007 <nimarahmanzadeh2007@gmail.com>
2026-08-05 14:03:09 +0330

    async and await with try and catch

---
+++ javascript_api/async_await_with_try_catch.js
@@ -0,0 +1,21 @@
+async function getUsers() {
+
+
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/userrrrr");
+
+        if (!response.ok) {
+            throw new Error("Request Failed!");
+        }
+
+        const data = await response.json();
+        console.log(data);
+    }
+    catch (error) {
+        console.log(error.message);
+    }
+
+
+}
+
+getUsers();
